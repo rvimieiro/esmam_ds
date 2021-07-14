43136ae0f5ff0b7059ac1c19a0658cf36eeda160
Pedro Gabriel Amorim Soares <pedrogabrielbhz@gmail.com>
2021-07-13 22:14:36 -0300

    rename files, start calculate_quality implementation

--- rule.py
+++ rule.py
@@ -1,5 +1,6 @@
 from enum import Enum
-from dataset_prototype import Dataset
+from dataset import Dataset
+import statsmodels.api as sm
 import os
 
 
@@ -10,16 +11,16 @@ class Baseline(Enum):
 
 class Rule:
     """Implement a rule. A rule is composed of an antecedent. This antecedent
-    covers a subgroup and a this subgroup has an associated survival function.
+    covers a subgroup and a this subgroup has an associated cover_survival function.
     """
 
     def __init__(self, Dataset, Baseline):
 
         self._antecedent: set = set()
         self._cover = None  # Poderia ser propriedade?
+        self.quality: float = None
         self.baseline: Baseline = Baseline
         self.Dataset: Dataset = Dataset
-        self.quality: float = None
 
     @property
     def antecedent(self) -> set:
@@ -57,6 +58,30 @@ class Rule:
         """Return the size of a Rule's cover."""
         return len(self.cover)
 
+    def calculate_quality(self):
+        """Calculate the Rule's quality according to Logrank test."""
+        self.set_cover()
+        cover = self.get_cover()
+
+        cover_survival = self.Dataset.survival
+        cover_survival = cover_survival[cover]
+
+        cover_status = self.Dataset.status
+        cover_status = cover_status[cover]
+
+        if self.baseline == Baseline.POPULATION:
+            pass
+        else:
+            pass
+
+        print('\nCover\n')
+        print(cover)
+        print('\nSurvival\n')
+        print(cover_survival)
+        print('\nStatus\n')
+        print(cover_status)
+        pass
+
     def __repr__(self):
         """Pretty-prints the rule."""
         return " \u2227 ".join(map(lambda x: "{} = {}".format(
@@ -79,6 +104,7 @@ if __name__ == "__main__":
     regra.add_item(ds.item_map[('tx', '0')])
     regra.add_item(ds.item_map[(('karnof', '90'))])
     regra.add_item(ds.item_map[('age', '[32.00,35.00)')])
-    regra.set_cover()
 
     print(regra)
+
+    regra.calculate_quality()
