15e81ade91a65f9939da2efd458fbb9219f76e38
Pedro Gabriel <pedrogabrielbhz@gmail.com>
2021-07-25 15:25:00 -0300

    implement quality calculation

--- rule.py
+++ rule.py
@@ -19,10 +19,8 @@ class Rule:
     """
 
     def __init__(self, Dataset, Baseline):
-
-        self._antecedent: set = set()
         self._cover = None
-        self.quality: float = None
+        self._antecedent: set = set()
         self.baseline: Baseline = Baseline
         self.Dataset: Dataset = Dataset
 
@@ -36,8 +34,8 @@ class Rule:
         self._antecedent = new_antecedent
 
     def add_item(self, item: int) -> None:
-        """Add item to antecedent. The item is referred by its integer index in
-        the Dataset's class item_map.
+        """Add item to antecedent. The item is referred by its integer index
+        in the Dataset's class item_map.
         """
         self._antecedent.add(item)
 
@@ -46,8 +44,9 @@ class Rule:
         integer-valued index in the Dataset's class item_map.
         """
         self._antecedent.remove(item)
+        # recalcular qualidade
 
-    def set_cover(self):
+    def set_cover(self) -> None:
         """Set the rule cover. A rule's cover is defined as the set of
         transactions where each transaction contain, at least,
         every item of the antecedent."""
@@ -57,11 +56,11 @@ class Rule:
         """Return Rule's cover."""
         return self.cover
 
-    def get_cover_size(self):
+    def get_cover_size(self) -> int:
         """Return the size of a Rule's cover."""
         return len(self.cover)
 
-    def population_quality(self):
+    def __population_quality(self) -> float:
         """Calculate rule's quality based on population comparison."""
         population_identifier = np.zeros(shape=len(self.Dataset.DataFrame))
 
@@ -79,7 +78,7 @@ class Rule:
         _, pvalue = sm.duration.survdiff(time, status, group)
         return 1 - pvalue
 
-    def complement_quality(self):
+    def __complement_quality(self) -> float:
         """Calculate rule's quality based on complement comparison."""
         group = np.zeros(shape=len(self.Dataset.DataFrame))
         np.put(group, self.get_cover(), 1)
@@ -89,36 +88,28 @@ class Rule:
         _, pvalue = sm.duration.survdiff(time, status, group)
         return 1 - pvalue
 
-    def calculate_quality(self):
+    def quality(self) -> float:
         """Calculate the Rule's quality according to Logrank test."""
-        # Discussao:
-        # 1. E se a cobertura eh vazia, onde isso deve ser testado?
-        #    Por enquanto coloquei qualidade = 0 se cobertura vazia.
-        # 2. Quais possiveis testes podemos fazer?
-        # 3. Etapas cruciais que devem ser verificadas ao longo do caminho
-        #    ateh aqui.
-
         self.set_cover()
         if len(self.cover) == 0:
-            self.quality = 0
+            return 0
         else:
             if self.baseline == Baseline.COMPLEMENT:
-                self.quality = self.complement_quality()
+                return self.__complement_quality()
             else:
-                self.quality = self.population_quality()
-        return
+                return self.__population_quality()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """Pretty-prints the rule."""
         return " \u2227 ".join(map(lambda x: "{} = {}".format(
             *self.Dataset.items_list[x]), self.antecedent)
-        ) + " \u2192 " + str(self.quality)
+        ) + " \u2192 " + str(self.quality())
 
 
 if __name__ == "__main__":
 
     pwd = os.getcwd()
-    path = pwd + '/datasets/actg320_disc.xz'
+    path = pwd + '/datasets/breast-cancer_disc.xz'
 
     ds = Dataset(path, "survival_time", "survival_status")
     ds.load_dataframe()
@@ -126,7 +117,8 @@ if __name__ == "__main__":
     ds.make_transaction_array()
 
     rule = Rule(ds, Baseline.COMPLEMENT)
-    for i in [1, 6, 16, 28, 38]:
-        rule.add_item(i)
-        rule.calculate_quality()
-        print(rule)
+    # for i in [1, 6, 16, 28, 38]:
+    # rule.add_item(i)
+    # print(rule)
+    rule.add_item(6)
+    print(rule)
